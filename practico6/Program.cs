6202f268a59829ad8a034cf31ccd01224f773ff7
vaninaze <vaninazee@gmail.com>
2023-05-18 19:54:42 -0300

    commit ejercicio 2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -10,10 +10,9 @@ Console.WriteLine("valor de b:"+b);
 
 //EJERICIO 1
 
-int intNum=0, aux=0, resto, invertido=0;
+int intNum=0, aux=0, resto, invertido=0, ceros=0;
 bool anda = false;
 string? numString="";
-int ceros=0;
 
 while (!anda)
 {
@@ -62,3 +61,41 @@ for(int i=0; i<2; i++)
 
 Console.WriteLine("El numero inicial es: " + intNum);
 Console.WriteLine("El numero invertido es: " + invertido);
+
+
+//EJERCICIO 2
+
+int intMenu=0, num1, num2;
+string? auxMenuString = "";
+bool menu = false;
+
+while (!menu || intMenu < 1 || intMenu > 4)
+{
+    Console.WriteLine("Que desea hacer?\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir");
+    auxMenuString = Console.ReadLine();
+    menu = int.TryParse(auxMenuString, out intMenu);
+    if(!menu || intMenu < 1 || intMenu > 4)
+    {
+        Console.WriteLine("Valor ingresado invalido");
+    }
+}
+
+Console.WriteLine("Num 1: ");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Num 2: ");
+num2 = int.Parse(Console.ReadLine());
+switch(intMenu)
+{
+    case 1: num1 += num2;
+            Console.WriteLine("La suma da: " + num1);
+            break;
+    case 2: num1 -= num2;
+            Console.WriteLine("La resta da: " + num1);
+            break;
+    case 3: num1 *= num2;
+            Console.WriteLine("La multiplicación da: " + num1);
+            break;
+    case 4: num1 /= num2;
+            Console.WriteLine("La división da: " + num1);
+            break;
+}
